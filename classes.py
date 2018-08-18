f0a84337b86d9ee202e032e586309a023bbaa13e
ABHISHEK KADAM <abhi.kadam62@gmail.com>
2018-08-18 21:49:26 +0530

    .

--- classes.py
+++ classes.py
@@ -18,15 +18,47 @@ class Dog():
         self.name = name
         self.age = age
     def sit(self):
-        print(self.name.title()+"is now sitting")
+        print(self.name.title()+" is now sitting")
     def roll(self):
-        print(self.name.title()+"has just rolled over")
+        print(self.name.title()+" has just rolled over")
 
 
 #MAKING AN INSTANCE FROM A CLASS
 my_dog = Dog('willie',6)
 print("My dogs name is "+ my_dog.name.title()+".")
 print("My dog is "+ str(my_dog.age) +" years old.")
+
+#ACCESSING ATTRIBUTES
+my_dog.name
+
+#CALLING METHODS
+my_dog.sit()
+my_dog.roll()
+
+#WE CAN CREATE AS MANY INSTANCES OF A CLASS AS WE WANT
+#IT DOES NOT MATTER IF WE GIVE THE SAME DATA IN THE CLASS PARAMETERS, ALL WE NEED TO DO IS GIVE THE
+# INSTANCE A UNIQUE VARIABLE NAME
+
+#EXCERCISE 1
+
+class Restaurant():
+    def __init__(self,name,cusine):
+        self.name = name
+        self.cusine = cusine
+
+    def describe_restaurant(self):
+        print("Welcome to "+self.name.title())
+        print("We serve delicious "+self.cusine.title()+" cusine")
+
+    def open_restaurant(self):
+        print("WE ARE NOW OPEN")
+
+restaurant = Restaurant('Casalobo','goan')
+restaurant_name = Restaurant.describe_restaurant()
+print(restaurant_name)
+
+
+
 
 
 
